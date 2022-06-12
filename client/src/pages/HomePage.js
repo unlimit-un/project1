f8a8451ce46756f2bb9eac0c2f9ec2a040260586
pjmin0230 <107266858+pjmin0230@users.noreply.github.com>
2022-06-13 00:40:39 +0700

    MHM

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 export const HomePage = () => {
   return (
     <>
       <div className="bg-blue-200 min-h-screen">
-        <Link to="/manager/" className="text-xl text-pink-500" > manager </Link>
+        
+        <h1> Maintenance works and Housekeeper's works 
+Management System </h1>
+
+        <Link to="/manager/" className="text-xl text-pink-500" > เข้าสู่หน้าหลัก </Link>
       </div>
     </>
   )
