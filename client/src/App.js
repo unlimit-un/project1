53ac7762638bf4f8a82f934971c1952078a753e5
unlimit <unlimit09@gmail.com>
2022-06-12 22:38:52 +0700

    add homepage

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,6 @@ function App() {
   return (
     <>
       <div className="bg-gray-100 min-h-screen">
-        <div className="flex justify-center items-center min-h-screen">
-          <h1 className="m-0 text-2xl text-black">Hello Project</h1>
-        </div>
         <Routes>
           <Route path='/' exac element={<HomePage/>}/>
         </Routes>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const HomePage = () => {
   return (
     <>
-      <div className="flex items-center justify-center bg-red-400 min-h-screen">
-        <h1 className="underline text-indigo-700">Home page</h1>
+      <div className="flex items-center justify-center bg-blue-200 min-h-screen">
+        <h1 className="font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-indigo-700 to-pink-500 italic">Home page</h1>
       </div>
     </>
   )
