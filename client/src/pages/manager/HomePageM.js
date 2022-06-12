32a2e0bafcf6ca8bb78ad632b6f2b676f5125fee
unlimit <unlimit09@gmail.com>
2022-06-13 00:13:37 +0700

    add sidebar

--- client/src/pages/manager/HomePageM.js
+++ client/src/pages/manager/HomePageM.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import Navbar from '../../components/manager/Navbar'
+import Navbar from '../../components/manager/NavbarM'
+import { SidebarM } from '../../components/manager/SidebarM'
 export const HomePageM = () => {
   return (
     <>
-        <div className="bg-blue-200 min-h-screen">
+        <div className="bg-blue-100 min-h-screen">
             <Navbar/>
+            <div className="row">
+                <div className="col-lg-2 col-md-3 col-12">
+                    <SidebarM/>
+                </div>
+                <div className="col-lg-10 col-md-9 col-12"></div>
+            </div>
         </div>
     </>
   )
