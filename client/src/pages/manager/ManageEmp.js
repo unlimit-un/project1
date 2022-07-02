d28b5b64e3b15006e1d0d41bdff1cc66ee291fe7
suphatarachai <suphatarachai1233@gmail.com>
2022-07-02 16:44:09 +0700

    commit

--- client/src/pages/manager/ManageEmp.js
+++ client/src/pages/manager/ManageEmp.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import Logo from '../../assets/Logo.jpg'
+import {  faUserTie  } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {Link} from 'react-router-dom'
 
 function ManageEmp() {
   return (
-    <div>ManageEmp</div>
+    <> 
+        <h1 className="text-2xl ms-4 mt-3"><FontAwesomeIcon icon={faUserTie}/> จัดการข้อมูลพนักงาน</h1>
+        <div className="card card-body mt-3">
+            <div className="flex justify-content flex-col">
+                <h2> จัดการข้อมูลแม่บ้าน</h2>
+               <div className="flex">
+                    <img src={Logo} alt="logo.png" className="w-40 h-40 rounded"/>    
+                    <div>
+                        <h1 className="ms-5">siriwimon</h1>
+                            <ul className="ms-5">
+                                <li>ชื่อผู้ใช้งาน :</li>
+                                <li>อีเมล :</li>
+                                <li>เบอร์โทร :</li>
+                            </ul>  
+                            <div className="d-flex justify-content-end ">
+                                <Link to="/" className="text-danger mx-auto"> จัดการพนักงาน </Link>
+                            </div>            
+                    </div>  
+               </div>
+                <hr />
+               <div className="flex">
+                    <img src={Logo} alt="logo.png" className="w-40 h-40 rounded"/>    
+                    <div>
+                        <h1 className="ms-5">siriwimon</h1>
+                            <ul className="ms-5">
+                                <li>ชื่อผู้ใช้งาน :</li>
+                                <li>อีเมล :</li>
+                                <li>เบอร์โทร :</li>
+                            </ul>              
+                    </div>  
+               </div>
+            </div>    
+        </div>
+         
+               
+    
+    </>
   )
 }
 
