780334fe966d28dc669811898f2a5e5c4a31225d
suphatarachai <suphatarachai1233@gmail.com>
2022-07-04 22:50:20 +0700

    commit

--- client/src/pages/manager/ManageEmp.js
+++ client/src/pages/manager/ManageEmp.js
@@ -10,7 +10,7 @@ function ManageEmp() {
         <h1 className="text-2xl ms-4 mt-3"><FontAwesomeIcon icon={faUserTie}/> จัดการข้อมูลพนักงาน</h1>
         <div className="card card-body mt-3">
             <div className="flex justify-content flex-col">
-                <h2> จัดการข้อมูลแม่บ้าน</h2>
+                <h2 className="text-2xl ms-4"><FontAwesomeIcon icon={faUserTie}/> จัดการข้อมูลแม่บ้าน</h2>
                <div className="flex">
                     <img src={Logo} alt="logo.png" className="w-40 h-40 rounded"/>    
                     <div className="w-full">
@@ -28,13 +28,31 @@ function ManageEmp() {
                 <hr />
                <div className="flex">
                     <img src={Logo} alt="logo.png" className="w-40 h-40 rounded"/>    
-                    <div>
+                    <div className="w-full">
                         <h1 className="ms-5">siriwimon</h1>
                             <ul className="ms-5">
                                 <li>ชื่อผู้ใช้งาน :</li>
                                 <li>อีเมล :</li>
                                 <li>เบอร์โทร :</li>
-                            </ul>              
+                            </ul> 
+                            <div className="d-flex justify-content-end ">
+                                <Link to="/" className="text-success"> จัดการพนักงาน </Link>
+                            </div>               
+                    </div>  
+               </div>
+               <hr/>
+               <div className="flex">
+                    <img src={Logo} alt="logo.png" className="w-40 h-40 rounded"/>    
+                    <div className="w-full">
+                        <h1 className="ms-5">siriwimon</h1>
+                            <ul className="ms-5">
+                                <li>ชื่อผู้ใช้งาน :</li>
+                                <li>อีเมล :</li>
+                                <li>เบอร์โทร :</li>
+                            </ul> 
+                            <div className="d-flex justify-content-end ">
+                                <Link to="/" className="text-success"> จัดการพนักงาน </Link>
+                            </div>               
                     </div>  
                </div>
             </div>    
