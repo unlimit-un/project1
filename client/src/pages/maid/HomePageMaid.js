80fddeb87b0dc7701a4ef0940c9f23e8fd4ae4f7
unlimit <unlimit09@gmail.com>
2022-07-19 14:03:48 +0700

    commit

--- client/src/pages/maid/HomePageMaid.js
+++ client/src/pages/maid/HomePageMaid.js
@@ -1,7 +1,6 @@
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import Cards from 
 
 const HomepageMaid = () => {
     return (
